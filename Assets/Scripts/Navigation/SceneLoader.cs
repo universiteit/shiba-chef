fc09b9a6d48e7a0574f90ad8306ab532788e83ef
Daniel <danielmolenaar@aol.nl>
2016-11-29 18:33:21 +0100

    Game finished

---
+++ Assets/Scripts/Navigation/SceneLoader.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour, IPointerClickHandler {
+
+    public string Scene;
+
+    public void OnPointerClick(PointerEventData eventData) {
+        if (Scene != null)
+            SceneManager.LoadScene(Scene);
+    }
+}
